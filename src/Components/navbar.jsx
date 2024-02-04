6d0a311dfc0b4a4deabab9eb52725ca3a1708241
Sandeep-Sam <rollisandeep10@gmail.com>
2024-02-04 12:10:12 +0530

    added files

---
+++ src/Components/navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div>
+        <header>
+            <div className="contact-details">
+                <div>9008637440</div>
+                <div>   </div>
+            </div>
+        </header>
+      <nav>
+
+      </nav>
+    </div>
+  )
+}
+
+export default navbar
